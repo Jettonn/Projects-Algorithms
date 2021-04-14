96fdb09908d81e159a24534841c16969b16bd0d1
Jeton Korenica <jetonkorenica@gmail.com>
2021-04-14 13:29:27 +0200

    SinglyLinkedLists with C#

---
+++ SinglyLinkedLists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SinglyLinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SLL singleLinkedLists = new SLL();
+            singleLinkedLists.AddFront(1);
+            singleLinkedLists.AddFront(2);
+            singleLinkedLists.AddFront(3);
+            singleLinkedLists.AddFront(4);
+            singleLinkedLists.PrintAllNodes();
+            Console.WriteLine(singleLinkedLists.Front());
+            singleLinkedLists.RemoveFront();
+            singleLinkedLists.PrintAllNodes();
+        }
+    }
+}
---
+++ SinglyLinkedLists/SLL.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace SinglyLinkedLists
+{
+    public class SLL
+    {
+        Node head = new Node(0);
+
+        public SLL()
+        {
+            head = null;
+        }
+
+        public Node AddFront(int value){
+            Node newNode = new Node(value);
+
+            newNode.next = head;
+
+            head = newNode;
+
+            return head;
+        }
+
+        public Node RemoveFront(){
+            if(head != null){
+                head = head.next;
+                return head;
+            }
+            return null;
+        }
+
+        public int? Front(){
+            if(!String.IsNullOrEmpty(head.data.ToString())){
+                return head.data;
+            }
+            return null;
+        }
+
+        public void PrintAllNodes(){
+            Node currentNode = head;
+            while (currentNode!=null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+        }
+    }
+}
