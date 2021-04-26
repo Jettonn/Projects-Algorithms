651f806a6e1dd012f0e153056a0efc249856dcdb
Jeton Korenica <jetonkorenica@gmail.com>
2021-04-26 13:46:24 +0200

    Recursion Todo1

---
+++ RecursionToDo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RecursionToDo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursive Sigma: Sum of n numbers: " + rSigma(6));
+            Console.WriteLine("Recursive Factorial: Products of n numbers: " + rFact(6));
+        }
+
+        static int rSigma(double n)
+        {
+            int number = (int)n;
+            if (number <= 0)
+                return 0;
+            if (number == 1)
+                return 1;
+            return number + rSigma(number - 1);
+        }
+
+        static int rFact(double n)
+        {
+            int number = (int)n;
+            if (number == 0)
+                return 1;
+            return number * rFact(n - 1);
+        }
+    }
+}
